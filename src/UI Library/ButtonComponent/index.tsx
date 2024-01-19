8056dbf8534be4a6111bec5fb026050de9a4d612
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2024-01-19 15:30:40 +0000

    bulding UI library

--- src/UI Library/ButtonComponent/index.tsx
+++ src/UI Library/ButtonComponent/index.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 
-const ButtonComponent = (props: any) => {
+const ButtonComponent = (props: { buttonText?: string }) => {
   const { buttonText } = props;
   return (
     <View>
-      <TouchableOpacity>
+      <TouchableOpacity style={styles.buttonCntnr}>
         <Text>ButtonComponent</Text>
-        <Text>{buttonText}</Text>
+        {/* <Text>{buttonText}</Text> */}
       </TouchableOpacity>
     </View>
   );
@@ -15,4 +15,11 @@ const ButtonComponent = (props: any) => {
 
 export default ButtonComponent;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonCntnr: {
+    borderWidth: 1,
+    padding: 10,
+    justifyContent: "center",
+    borderRadius: 10,
+  },
+});
---
+++ src/UI Library/FormComponent/index.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const FormComponent = () => {
+  return (
+    <View>
+      <Text>FormComponent</Text>
+    </View>
+  );
+};
+
+export default FormComponent;
+
+const styles = StyleSheet.create({});
