07484c7f3b1ed7f54c1b7ebfddfaa3b0f237f717
olatunjiemanuel <olatunjiemanuel15@gmail.com>
2024-05-22 21:32:40 +0100

    fixing virtualized list bug, removed scroll view as flatlist already functioning

--- src/UI Library/CategorySelector/index.tsx
+++ src/UI Library/CategorySelector/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View, ScrollView, FlatList } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import React from "react";
 import CategoryComponent from "../CategoryComponent";
 
@@ -11,25 +11,23 @@ const CategorySelector = <T extends (t: string) => void>(props: {
   const Categories = ["Science", "Drama", "Mystery", "Comedy"];
 
   return (
-    <ScrollView horizontal contentContainerStyle={styles.sliderCntnr}>
-      <View>
-        <FlatList
-          horizontal
-          showsVerticalScrollIndicator={false}
-          data={Categories}
-          keyExtractor={(index) => index.toString()}
-          renderItem={({ item }) => (
-            <CategoryComponent
-              selected={selectedCategory === item}
-              CategoryText={item}
-              onPress={() => {
-                onSelectCategory(item);
-              }}
-            />
-          )}
-        />
-      </View>
-    </ScrollView>
+    <View style={styles.sliderCntnr}>
+      <FlatList
+        horizontal
+        showsVerticalScrollIndicator={false}
+        data={Categories}
+        keyExtractor={(index) => index.toString()}
+        renderItem={({ item }) => (
+          <CategoryComponent
+            selected={selectedCategory === item}
+            CategoryText={item}
+            onPress={() => {
+              onSelectCategory(item);
+            }}
+          />
+        )}
+      />
+    </View>
   );
 };
 
